415e8f7878f8a3025eebd5d9ef9cfbeb8952fc05
Diego Almeida <diego.almeida@outlook.com>
2020-11-02 14:37:05 -0300

    Adicionada as Rotas GET, POST para vendas

--- api-vendas-v2/index.js
+++ api-vendas-v2/index.js
@@ -1,19 +1,26 @@
 const sqlite3 = require('sqlite3');
 const express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 const HTTP_PORT = 3003
 app.listen(HTTP_PORT, () => {
     console.log("Servidor iniciado na porta " + HTTP_PORT);
 });
 
-const db = new sqlite3.Database('./api_database.db', (err) => {
+/**
+ * Cria a tabela de vendas
+ */
+const db = new sqlite3.Database('./api.db', (err) => {
     if(err){
         console.log("Erro ao comunicar com o banco de dados " + err.message);
     } else {
         db.run('CREATE TABLE vendas( \
             venda_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, \
-            valor_venda DECIMAL(10,2) NOT NULL, \
+            valor_venda FLOAT NOT NULL, \
             produto_id INTEGER NOT NULL,\
             quantidade INTEGER NOT NULL\
             )', (err) => {
@@ -22,4 +29,49 @@ const db = new sqlite3.Database('./api_database.db', (err) => {
                 }
             });
     }
+});
+
+/**
+ * Lista uma venda
+ */
+app.get("/vendas/:id", (req, res, next) => {
+    var params = [req.params.id]
+    db.get(`SELECT * FROM vendas WHERE venda_id = ?`, [req.params.id], (err, row) => {
+        if (err) {
+            res.status(400).json({"error": err.message});
+            return;
+        }
+        res.status(200).json(row);
+    });
+});
+
+/**
+ * Lista todas as vendas
+ */
+app.get("/vendas", (req, res, next) => {
+    db.all("SELECT * FROM vendas", [], (err, rows) => {
+        if (err) {
+            res.status(400).json({"error": err.message});
+            return;
+        }
+        res.status(200).json({rows});
+    });
+});
+
+/**
+ * Insere uma venda
+ */
+app.post("/vendas", (req, res, next) => {
+    var reqBody = req.body;
+    db.run(`INSERT INTO vendas (valor_venda, produto_id, quantidade) VALUES (?,?,?)`, 
+    [reqBody.valor_venda, reqBody.produto_id, reqBody.quantidade],
+    function (err, result) {
+        if (err) {
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.status(201).json({
+            "venda_id": this.lastID
+        })
+    });
 });
